1f5a24ea8662a90380294f4e3f89f84d5d299d64
Krishnadev1999 <iamkrishnadev@1999gmail.com>
2020-02-19 07:49:24 +0530

    alsdflasd

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './sign-in-and-sign-up.style.scss'
+
+const SignInAndSignUp = () => (
+    <div className="sign-in-and-sign-up">
+        Sign in 
+
+    </div>
+)
+
+export default SignInAndSignUp;
